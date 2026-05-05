c878052dbe52bdd2fc0fee7867769176c6389880
Max <marleyjodici@gmail.com>
2026-05-05 14:16:16 -0700

    updated variables

---
+++ src/main/java/Reservation.java
@@ -0,0 +1,12 @@
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend){
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+
+
+    }
+}
