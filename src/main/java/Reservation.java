09d47f612dd56f09480bfa6b848bbfe85b32a934
Max <marleyjodici@gmail.com>
2026-05-05 15:10:02 -0700

    updated variables

--- src/main/java/Reservation.java
+++ src/main/java/Reservation.java
@@ -2,10 +2,13 @@ public class Reservation {
     private String roomType;
     private int numberOfNights;
     private boolean isWeekend;
+
     public Reservation(String roomType, int numberOfNights, boolean isWeekend){
+
         this.roomType = roomType;
         this.numberOfNights = numberOfNights;
         this.isWeekend = isWeekend;
+
         public String getroomType(){
             return roomType;
         }
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,13 +1,17 @@
 public class Room {
+
     private int numberOfBeds;
     private double price;
     private boolean occupied;
     private boolean dirty;
+
     public Room(int numberOfBeds, double price,boolean occupied,boolean dirty ) {
+
         this.numberOfBeds = numberOfBeds;
         this.price = price;
         this.occupied = occupied;
         this.dirty = dirty;
+
         public int getnumberOfBeds() {
             return numberOfBeds;
         }
