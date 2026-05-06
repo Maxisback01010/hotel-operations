224875373c1221daee09a2e6d63ba7bf37e7b583
Max <marleyjodici@gmail.com>
2026-05-06 15:50:37 -0700

    updated variables

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,35 +1,33 @@
 public class Room {
 
-    private int numberOfBeds;
-    private double price;
-    private boolean occupied;
-    private boolean dirty;
+        private int numberOfBeds;
+        private double price;
+        private boolean occupied;
+        private boolean dirty;
 
-    public Room(int numberOfBeds, double price,boolean occupied,boolean dirty ) {
+    public Room( int numberOfBeds, double price, boolean occupied, boolean dirty){
 
-        this.numberOfBeds = numberOfBeds;
-        this.price = price;
-        this.occupied = occupied;
-        this.dirty = dirty;
+            this.numberOfBeds = numberOfBeds;
+            this.price = price;
+            this.occupied = occupied;
+            this.dirty = dirty;
 
-        public int getnumberOfBeds() {
-            return numberOfBeds;
-        }
-        public double getprice(){
-            return price;
-        }
-        public boolean isoccupied(){
-            return occupied;
-        }
-        public boolean isdirty(){
-            return dirty;
-        }
-        public boolean isAvailable(){
-            return !occupied && !dirty;
-        }
+            public int getnumberOfBeds () {
+                return numberOfBeds;
+            }
+            public double getprice () {
+                return price;
+            }
+            public boolean isoccupied () {
+                return occupied;
+            }
+            public boolean isdirty () {
+                return dirty;
+            }
+            public boolean isAvailable () {
+                return !occupied && !dirty;
+            }
+
     }
-
-
-
 
 }
